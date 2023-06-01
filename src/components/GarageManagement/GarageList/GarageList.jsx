c1b8218f183f929dcf6565136bcde45776abeac4
HaoPro <haopka.cnttvj@gmail.com>
2023-06-02 00:04:01 +0700

    update

--- src/components/GarageManagement/GarageList/GarageList.jsx
+++ src/components/GarageManagement/GarageList/GarageList.jsx
@@ -32,6 +32,7 @@ const GarageManagementList = () => {
     page: 1,
     pageSize: 10,
   });
+  const [totalItems, setTotalItems] = useState(0);
   const { Search } = Input;
   const options = [
     {
@@ -60,8 +61,8 @@ const GarageManagementList = () => {
   const columns = [
     {
       title: '#',
-      dataIndex: 'STT',
-      key: 'STT',
+      dataIndex: 'id',
+      key: 'id',
     },
     {
       title: 'Name',
@@ -103,46 +104,52 @@ const GarageManagementList = () => {
 
   const [data, setData] = useState([]);
   useEffect(() => {
-    const jwt = localStorage.getItem('jwt');
-    const requestOptions = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${jwt}`,
-      },
-      redirect: 'follow',
-    };
-    let filterParams = {};
-    if (isActived_2 === '') {
-    } else if (isActived_2 === 'active') {
-      filterParams['status][$eq]'] = 'active';
-    } else if (isActived_2 === 'inactive') {
-      filterParams['status][$eq]'] = 'inactive';
-    }
-    if (isActived_1 === 'Name') {
-      filterParams['name][$contains]'] = searchText;
-    } else if (isActived_1 === 'Email') {
-      filterParams['email][$contains]'] = searchText;
-    }
+    const fetchData = async () => {
+      try {
+        const jwt = localStorage.getItem('jwt');
+        const requestOptions = {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${jwt}`,
+          },
+          redirect: 'follow',
+        };
+        let filterParams = {};
+        if (isActived_2 === '') {
+        } else if (isActived_2 === 'active') {
+          filterParams['status][$eq]'] = 'active';
+        } else if (isActived_2 === 'inactive') {
+          filterParams['status][$eq]'] = 'inactive';
+        }
+        if (isActived_1 === 'Name') {
+          filterParams['name][$contains]'] = searchText;
+        } else if (isActived_1 === 'Email') {
+          filterParams['email][$contains]'] = searchText;
+        }
 
-    const filters = Object.entries(filterParams)
-      .map(([key, value]) => `filters[${key}]=${encodeURIComponent(value)}`)
-      .join('&');
-    const paginationParams = `pagination[page]=${pagination.page}&pagination[pageSize]=${pagination.pageSize}`;
-    const apiUrl = `http://localhost:1337/api/garages?${filters}&${paginationParams}&populate=owner, `;
-    console.log(apiUrl);
-    fetch(apiUrl, requestOptions)
-      .then(response => response.json())
-      .then(result => {
-        if (result.data) {
+        const filters = Object.entries(filterParams)
+          .map(([key, value]) => `filters[${key}]=${encodeURIComponent(value)}`)
+          .join('&');
+        const paginationParams = `pagination[page]=${pagination.page}&pagination[pageSize]=${pagination.pageSize}`;
+        const apiUrl = `http://localhost:1337/api/garages?${filters}&${paginationParams}&populate=owner, `;
+        const response = await fetch(apiUrl, requestOptions);
+        const result = await response.json();
+        if (response.ok) {
           const arrayNew = result.data.map(item => ({
             ...item.attributes,
+            id: item.id,
           }));
           setData(arrayNew);
           console.log(data);
+          setTotalItems(result.meta.pagination.total);
         }
-      })
-      .catch(error => console.log('error', error));
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchData();
   }, [searchText, isActived_1, isActived_2, pagination]);
   useEffect(() => {
     const fetchData = async () => {
@@ -301,7 +308,7 @@ const GarageManagementList = () => {
               pagination={{
                 current: pagination.page,
                 pageSize: pagination.pageSize,
-                total: data.length,
+                total: totalItems,
                 onChange: handlePagination,
               }}
               dataSource={data.map((data, index) => ({
